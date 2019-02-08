091a1711d13e92a27d2daa9833573824730b9237
Tingbin Huang <tingbinhuang@Tingbins-MacBook-Pro.local>
2019-02-08 15:56:14 -0800

    Make some changes

--- server/index.js
+++ server/index.js
@@ -9,31 +9,29 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/../client/dist'));
 
-// app.post('/menu', (req, res) => {
+app.post('/menu', (req, res) => {
 
 
-//     for (let i = 0; i < 100; i++) {
-//         let name = food[i];
-//         let description = faker.lorem.sentence();
-//         let price = Math.random() * (25 - 9) + 9;
+    for (let i = 0; i < 100; i++) {
+        let name = food[i];
+        let description = faker.lorem.sentence();
+        let price = Math.random() * (25 - 9) + 9;
 
+        let params = [name, description, price];
 
+        let queryStr = 'insert into dinner(name, description,price) \
+        values (?,?,?)';
 
-//         let params = [name, description, price];
+        con.connection.query(queryStr, params, function (err, result) {
+            if (err) {
+                console.log("error message: ", err);
+                return;
+            }
+            console.log('done: ', result);
+        });
 
-//         let queryStr = 'insert into dinner(name, description,price) \
-//         values (?,?,?)';
-
-//         con.connection.query(queryStr, params, function (err, result) {
-//             if (err) {
-//                 console.log("error message: ", err);
-//                 return;
-//             }
-//             console.log('done: ', result);
-//         });
-
-//     }
-// });
+    }
+});
 
 app.get("/getMenu", function (req, res) {
 
