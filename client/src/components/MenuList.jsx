51f789eb79bfb214408299b7f36296079ad08b75
Tingbin Huang <tingbinhuang@Tingbins-MacBook-Pro.local>
2019-02-16 14:46:34 -0800

    Refact MenuList for view full menu and collapse menu buttons

--- client/src/components/MenuList.jsx
+++ client/src/components/MenuList.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 import MenuItem from './MenuItem.jsx';
 import styled from 'styled-components';
-import jump from 'jump.js';
-
-
 
 class MenuList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isExpanded: false
+      isExpanded: false,
+      showFullMenu: false,
+      clickFullMenu: false
     }
-
     this.handleToggle = this.handleToggle.bind(this);
   }
 
   handleToggle(e) {
     e.preventDefault();
     this.setState({
-      isExpanded: !this.state.isExpanded
+      isExpanded: !this.state.isExpanded,
+      showFullMenu: !this.state.showFullMenu,
+      clickFullMenu: !this.state.clickFullMenu
     })
     document.documentElement.scrollTop = 0;
   }
@@ -30,17 +30,25 @@ class MenuList extends React.Component {
     const arr4 = this.props.lists.slice(60, 80);
     const arr5 = this.props.lists.slice(80, 100);
     const lists = [arr2, arr3, arr4, arr5];
+    const imageURL = "https://a.singleplatform.com/OpenTable/fogo-de-chao-15/provided_by_2.png?sp_dt=20190212";
 
     return (
       <div>
 
         <TopLevel>
           <MenuItem items={arr1} />
-
           <ItemLine></ItemLine>
-          {/* <Blur></Blur> */}
-          <Space />
+          <Blur showMenu={this.state.showFullMenu}>
+            <FullMenuButton type='button' value='full-menu' onClick={this.handleToggle}>View all details</FullMenuButton>
+            <LastLine />
 
+            <LastUpdate check={this.state.clickFullMenu}>
+              <DateForLastUpdate>Last Updated: Febuary 16, 2019</DateForLastUpdate>
+              <Image src={imageURL} />
+            </LastUpdate>
+
+          </Blur>
+          <Space />
         </TopLevel>
 
         <MainBox expanded={this.state.isExpanded}>
@@ -54,10 +62,17 @@ class MenuList extends React.Component {
             );
           })}
         </MainBox>
+        <HiddenCollapseButton showButton={this.state.showFullMenu}>
+          <ClosingText>Thank you for coming!</ClosingText>
+          <CollapseButton onClick={this.handleToggle}>Collapse menu</CollapseButton>
+          <BottomLine />
+          <LastUpdate check={this.state.clickFullMenu}>
+            <DateForLastUpdate>Last Updated: Febuary 16, 2019</DateForLastUpdate>
+            <Image src={imageURL} />
+          </LastUpdate>
 
-        <ClosingText>Thank you for coming!</ClosingText>
-        <div><CollapseButton onClick={this.handleToggle}>Collapse menu</CollapseButton></div>
-        <Space /><Space />
+          <Space /><Space />
+        </HiddenCollapseButton>
 
       </div>
     );
@@ -73,6 +88,15 @@ const Space = styled.br``;
 const ItemLine = styled.div`
   padding-top: 10px;
   padding-bottom: 10px;
+  width:900px;
+  border-bottom: 1px solid #d8d9db;
+`;
+
+const BottomLine = styled.div`
+  padding-top: 10px;
+  padding-bottom: 10px;
+  height:80px;
+  width:900px;
   border-bottom: 1px solid #d8d9db;
 `;
 
@@ -80,13 +104,70 @@ const TopLevel = styled.div`
   flexBasis: 80%; 
 `;
 
+const LastLine = styled.div`
+  padding-top: 5px;
+  padding-bottom: 5px;
+  border-bottom: 1px solid #d8d9db;
+  position: relative;
+  top:290px;
+  width: 900px;
+
+`;
+
+const LastUpdate = styled.div`
+  display:flex;
+  position: relative;
+  top: ${(props) => props.check ? '' : '300px'}; 
+  ${'' /* height: 30px; */}
+  font-size: xx-small;
+  font-family:Quicksand;
+  font-weight: bolder;
+`;
+
+const DateForLastUpdate = styled.p`
+  ${'' /* flex: 1; */}
+  font-size: xx-small;
+  font-family:Quicksand;
+  font-weight: bolder; 
+`;
+
+const Image = styled.img`
+  position: relative;
+  left: 460px;
+  padding-top: 13px;
+  padding-bottom: 16px;
+`;
+
+const FullMenuButton = styled.button`
+  font-weight: bolder;
+  cursor: pointer;
+  background-color: white;
+  border: 1px solid #d8d9db;
+  font-size: smaller;
+  font-family: Quicksand;
+  font-weight: bold;
+  padding: 15px;
+  position: relative;
+  left: 280px;
+  right: 160px;
+  top:300px;
+  width: 300px;
+  high: 30px;
+  margin: 10px;
+  margin-bottom: 30px;
+  outline: none;
+  &:hover ${FullMenuButton} {
+    border: 2px solid #DA3743;
+  }  
+`;
+
 const Blur = styled.div`
   content: " ";
-  display: block;
+  display: ${(props) => props.showMenu ? 'none' : 'block'};
   position: relative;
   bottom: 300px;
   height: 300px;
-  background-image: linear-gradient(to top,#ff4,rgba(255,255,255,0));
+  background-image: linear-gradient(to top,#fff,rgba(255,255,255,0));
   background-color: rgba(255,255,255,.2);
 `;
 
@@ -95,25 +176,31 @@ const MainBox = styled.div`
   display: ${(props) => props.expanded ? 'block' : 'none'};
 `;
 
+const HiddenCollapseButton = styled.div`
+  display: ${(props) => props.showButton ? 'block' : 'none'};
+`;
+
 const ClosingText = styled.div`
   font-family: Quicksand;
   font-weight: bolder;
 `;
 
 const CollapseButton = styled.button`
-  zIndex: 20;
+  ${'' /* zIndex: 20; */}
   font-weight: bolder;
   cursor: pointer;
   background-color: white;
   border: 1px solid #d8d9db;
   font-size: smaller;
   font-family: Quicksand;
-  font-weight: bolder;
+  font-weight: bold;
   padding: 15px;
+
   position: absolute;
-  left: 380px;
+
+  left: 280px;
   right: 160px;
-  width: calc(30% - 70px);
+  width: 300px;
   high: 30px;
   margin: 10px;
   margin-bottom: 30px;
